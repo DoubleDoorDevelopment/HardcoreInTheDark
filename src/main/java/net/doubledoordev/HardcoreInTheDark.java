c4e3a0bfe55bd8319f1a4618878b1acf52cce2af
Claycorp <Claycorp@doubledoordev.net>
2020-04-12 02:36:18 -0500

    Done mod is done? Hope everyone likes ded.

---
+++ src/main/java/net/doubledoordev/HardcoreInTheDark.java
@@ -0,0 +1,114 @@
+package net.doubledoordev;
+
+import java.io.File;
+import java.util.Date;
+import java.util.UUID;
+
+import org.apache.logging.log4j.Logger;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerInteractionManager;
+import net.minecraft.server.management.UserListBansEntry;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.GameType;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import com.mojang.authlib.GameProfile;
+
+@Mod(
+        modid = HardcoreInTheDark.MOD_ID,
+        name = HardcoreInTheDark.MOD_NAME,
+        version = HardcoreInTheDark.VERSION,
+        acceptableRemoteVersions = "*"
+)
+public class HardcoreInTheDark
+{
+
+    public static final String MOD_ID = "hardcoreinthedark";
+    public static final String MOD_NAME = "Hardcore In The Dark";
+    public static final String VERSION = "1.0.0";
+
+    /**
+     * This is the instance of your mod as created by Forge. It will never be null.
+     */
+    @Mod.Instance(MOD_ID)
+    public static HardcoreInTheDark INSTANCE;
+    static Logger log;
+
+    /**
+     * This is the first initialization event. Register tile entities here.
+     * The registry events below will have fired prior to entry to this method.
+     */
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent event)
+    {
+        log = event.getModLog();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onPlayerDeath(LivingHurtEvent event)
+    {
+        if (event.getEntity() instanceof EntityPlayerMP && event.getEntityLiving().getHealth() <= event.getAmount())
+        {
+            int lightLevel = event.getEntityLiving().getEntityWorld().getLight(event.getEntityLiving().getPosition(), false);
+            if (lightLevel <= ModConfig.unsafeLight)
+                //True = SP.
+                if (event.getEntity().world.isRemote)
+                {
+                    ((EntityPlayerMP) event.getEntity()).setGameType(GameType.SPECTATOR);
+                    ((EntityPlayerMP) event.getEntity()).sendStatusMessage(new TextComponentString(ModConfig.deathMessage), false);
+                }
+                else
+                {
+                    if (lightLevel <= ModConfig.unsafeLight)
+                    {
+                        MinecraftServer server = event.getEntityLiving().getServer();
+                        EntityPlayerMP playerMP = (EntityPlayerMP) event.getEntity();
+                        UUID playerUUID = playerMP.getUniqueID();
+
+                        if (ModConfig.shouldSpectateOnServer)
+                        {
+                            playerMP.setGameType(GameType.SPECTATOR);
+                            server.getWorld(playerMP.dimension).getGameRules().setOrCreateGameRule("spectatorsGenerateChunks", "false");
+
+                            ((EntityPlayerMP) event.getEntity()).sendStatusMessage(new TextComponentString(ModConfig.deathMessage), false);
+                        }
+
+                        if (ModConfig.shouldBanOnServer)
+                        {
+                            Date thisInstant = new Date();
+                            Date endDate = new Date(ModConfig.banTime * 1000 + thisInstant.getTime());
+                            UserListBansEntry userlistbansentry =
+                                    new UserListBansEntry(((EntityPlayerMP) event.getEntity()).getGameProfile(), thisInstant, "HardcoreInTheDark", endDate,
+                                            String.format(ModConfig.banMessage, lightLevel));
+                            server.getPlayerList().getBannedPlayers().addEntry(userlistbansentry);
+
+                            playerMP.connection.disconnect(new TextComponentString(ModConfig.disconnectMessage));
+                        }
+
+                        //This whole mod is fairly simple to understand except why this is all done.
+                        if (ModConfig.shouldDeletePlayerData)
+                        {
+                            // We need an unobtainable UUID to make a player with as we don't want to fuck up everyone.
+                            GameProfile emptyProfile = new GameProfile(null, "YouFoundAnEasterEgg<3");
+                            EntityPlayerMP emptyPlayer = new EntityPlayerMP(server, server.getWorld(0), emptyProfile, new PlayerInteractionManager(server.getWorld(0)));
+                            // Then because you can't directly delete player data we indirectly delete it by copying a non-existing player's fresh data over our player.
+                            playerMP.copyFrom(emptyPlayer, false);
+
+                            //Then because advancements are part of the content these days and add things to the player data we need to clear these also otherwise...
+                            // the recipe book breaks and you can't get stuff back for it. So we direct delete the file and then force a reload, due to the file not existing a blank is created.
+                            File advancementPlayerData = new File(server.getWorld(0).getSaveHandler().getWorldDirectory(), "advancements/" + playerUUID + ".json");
+                            advancementPlayerData.delete();
+                            playerMP.getAdvancements().reload();
+                            //Absolute fukin magic this is...
+                        }
+                    }
+                }
+        }
+    }
+}
---
+++ src/main/java/net/doubledoordev/ModConfig.java
@@ -0,0 +1,63 @@
+package net.doubledoordev;
+
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import static net.doubledoordev.HardcoreInTheDark.MOD_ID;
+
+@Config(modid = MOD_ID, category = "All")
+@Mod.EventBusSubscriber(modid = MOD_ID)
+public class ModConfig
+{
+    @Config.Comment({"This level and below are unsafe, dying in this light level acts like hardcore."
+    })
+    @Config.RangeInt(min = 0, max = 16)
+    public static int unsafeLight = 7;
+
+    @Config.Comment({"Message Player gets after dying in an unsafe light level."
+    })
+    public static String deathMessage = "Whoops, you died in an unsafe area! Perhaps next time you will be more careful.";
+
+    @Config.Comment({"Should players be banned on a server?"
+    })
+    public static boolean shouldBanOnServer = false;
+
+    @Config.Comment({"Should players be set to Spectator on a server?"
+    })
+    public static boolean shouldSpectateOnServer = true;
+
+    @Config.Comment({"Should the player data for this player be deleted on death? WARNING: IRREVERSABLE!"
+    })
+    public static boolean shouldDeletePlayerData = false;
+
+    @Config.Comment({"How long a user is banned for IN REAL TIME on a server. Time in seconds!",
+            "60 = 1 Minute, 3600 = 1 hour, 43200 = 12 horus, 86400 =  24 hours"
+    })
+    @Config.RangeInt(min = 0, max = Integer.MAX_VALUE)
+    public static int banTime = 3600;
+
+    @Config.Comment({"Message used when player is kicked on the ban."
+    })
+    public static String disconnectMessage = "YOU DIED! YOU BANNED! YOU BAD!";
+
+    @Config.Comment({"Ban message user gets when trying to connect again while banned.",
+            "Use %s for the light level the user died at."
+    })
+    public static String banMessage = "Died at or below the unsafe light level (7) was %s";
+
+    @Mod.EventBusSubscriber
+    public static class SyncConfig
+    {
+        @SubscribeEvent
+        public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event)
+        {
+            if (event.getModID().equals(MOD_ID))
+            {
+                ConfigManager.sync(MOD_ID, Config.Type.INSTANCE);
+            }
+        }
+    }
+}
